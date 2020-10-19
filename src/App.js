45b05728f03d6c87ac102ac4278322cef0487e27
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-19 10:22:54 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component{
-  
+  state = { advice : ''};
+
+  componentDidMount(){
+    console.log('component did  mount');
+  }
   render(){
     return(
       <h1>heloo</h1>
